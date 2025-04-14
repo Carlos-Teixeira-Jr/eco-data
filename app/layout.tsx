d8749627b8b9e32258acf9495f8b5f756cda6120
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2025-04-14 12:42:06 -0300

    chore: preparing to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { Breadcrumb } from "./components/navigation/Breadcrumb";
 import Footer from "./components/navigation/Footer";
 import Header from "./components/navigation/Header";
